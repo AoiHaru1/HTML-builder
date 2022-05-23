9483d2b27b53cb61edceb76651324c861eae00b2
Aoi Haru <snakeinwater1@gmail.com>
2022-05-23 15:37:11 +0300

    fix: last task fix

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,12 +5,16 @@ fs.writeFile('./05-merge-styles/project-dist/bundle.css', '', () => { });
 
 const bundleFunc = async () => {
   const files = await fsPromises.readdir('./05-merge-styles/styles', { withFileTypes: true });
-  files.forEach(x => {
+  files.forEach((x, i) => {
     const ext = x.name.split('.');
     if (ext[ext.length - 1] === 'css') {
       const stream = fs.ReadStream(`./05-merge-styles/styles/${x.name}`);
       stream.on('data', (data) => {
-        fs.writeFile('./05-merge-styles/project-dist/bundle.css', `\n\n${data}`, { flag: 'a+' }, () => { });
+        if (!i) {
+          fs.writeFile('./05-merge-styles/project-dist/bundle.css', `${data.toString()}`, { flag: 'a+' }, () => { });
+        } else {
+          fs.writeFile('./05-merge-styles/project-dist/bundle.css', `\n\n${data.toString()}`, { flag: 'a+' }, () => { });
+        }
       });
     }
   });
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,6 +5,7 @@ const PATH = require('path');
 const bundlePath = PATH.resolve(__dirname, 'project-dist');
 const stylesPath = PATH.resolve(__dirname, 'styles');
 
+
 fsPromises.mkdir(bundlePath, { recursive: true });
 
 
@@ -13,12 +14,16 @@ fs.writeFile(bundlePath + '/style.css', '', () => { });
 
 const cssBundle = async () => {
   const files = await fsPromises.readdir(stylesPath, { withFileTypes: true });
-  files.forEach(x => {
+  files.forEach((x, i) => {
     const ext = x.name.split('.');
     if (ext[ext.length - 1] === 'css') {
       const stream = fs.ReadStream(stylesPath + `/${x.name}`);
       stream.on('data', (data) => {
-        fs.writeFile(bundlePath + '/style.css', `\n\n${data}`, { flag: 'a+' }, () => { });
+        if (!i) {
+          fs.writeFile(bundlePath + '/style.css', `${data.toString()}`, { flag: 'a+' }, () => { });
+        } else {
+          fs.writeFile(bundlePath + '/style.css', `\n\n${data.toString()}`, { flag: 'a+' }, () => { });
+        }
       });
     }
   });
@@ -26,9 +31,7 @@ const cssBundle = async () => {
 
 cssBundle();
 
-
 fsPromises.mkdir(bundlePath + '/assets', { recursive: true });
-
 
 const listOfFiles = async (path, outputPath) => {
   const files = await fsPromises.readdir(path, { withFileTypes: true });
@@ -36,7 +39,9 @@ const listOfFiles = async (path, outputPath) => {
     if (file.isFile()) {
       await fsPromises.copyFile(path + `/${file.name}`, outputPath + `/${file.name}`);
     } else {
-      fsPromises.mkdir(bundlePath + '/assets' + `/${file.name}`, { recursive: true });
+      await fsPromises.mkdir(bundlePath + '/assets' + `/${file.name}`, { recursive: true });
+      await fsPromises.rm(outputPath + `/${file.name}`, { recursive: true });
+      await fsPromises.mkdir(bundlePath + '/assets' + `/${file.name}`, { recursive: true });
       await listOfFiles(path + `/${file.name}`, outputPath + `/${file.name}`);
     }
   }
@@ -47,20 +52,21 @@ listOfFiles(__dirname + '/assets', __dirname + '/project-dist' + '/assets');
 
 const stream = fs.ReadStream(PATH.resolve(__dirname, 'template.html'));
 
+fs.writeFile(bundlePath + '/index.html', '', () => { });
+
 const htmlBundle = async () => {
   let htmlString;
-  stream.on('data', (data) => {
+  stream.on('data', async (data) => {
     htmlString = data.toString();
-  });
-
-  await stream.on('end', async () => {
     const files = await fsPromises.readdir(__dirname + '/components', { withFileTypes: true });
     for (const file of files) {
       const copyContentStream = fs.ReadStream(PATH.resolve(__dirname, 'components', file.name));
+      copyContentStream.on('data', async (data) => {
+        const regex = new RegExp(`{{${file.name.split('.')[0]}}}`, 'g');
+        htmlString = await htmlString.replace(regex, data.toString());
+      });
 
-      copyContentStream.on('data', (data) => {
-        const regex =  new RegExp(`{{${file.name.split('.')[0]}}}`,'g');
-        htmlString = htmlString.replace(regex, data.toString());
+      copyContentStream.on('end', () => {
         fs.writeFile(bundlePath + '/index.html', htmlString, () => { });
       });
     }
